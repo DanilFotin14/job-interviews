6aa8484e4995826744fb52094bcdc4c9f6859366
Danil <fot1722tof@gmail.com>
2025-09-10 09:05:25 +0300

    add create interview page

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,4 +1,89 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { getAuth } from 'firebase/auth'
+import { getFirestore, setDoc, doc } from 'firebase/firestore'
+import type { IInterview } from '@/interfaces'
+import { v4 as uuidv4 } from 'uuid'
+
+const db = getFirestore()
+const router = useRouter()
+
+const company = ref<string>('')
+const vacancyLink = ref<string>('')
+const hrName = ref<string>('')
+const contactTelegram = ref<string>('')
+const contactWhatsApp = ref<string>('')
+const contactPhone = ref<string>('')
+
+const loading = ref<boolean>(false)
+
+const addNewInterview = async (): Promise<void> => {
+  loading.value = true
+  const payload: IInterview = {
+    id: uuidv4(),
+    company: company.value,
+    vacancyLink: vacancyLink.value,
+    hrName: hrName.value,
+    contactTelegram: contactTelegram.value,
+    contactWhatsApp: contactWhatsApp.value,
+    contactPhone: contactPhone.value,
+    createdAt: new Date(),
+  }
+
+  const userId = getAuth().currentUser?.uid
+
+  if (userId) {
+    await setDoc(doc(db, `users/${userId}/interviews`, payload.id), payload).then(() => {
+      router.push('/list')
+    })
+  }
+}
+
+const disabledSaveButton = computed<boolean>(() => {
+  return !company.value || !vacancyLink.value || !hrName.value
+})
+</script>
 <template>
-  <h1>Home</h1>
+  <div class="content-interview">
+    <app-card>
+      <template #title>New Interview</template>
+      <template #content>
+        <app-input-text class="input mb-3" placeholder="Company" v-model="company" />
+        <app-input-text
+          v-model="vacancyLink"
+          class="input mb-3"
+          placeholder="Direct link to vacancy"
+        />
+        <app-input-text v-model="hrName" class="input mb-3" placeholder="Contact HR(Name)" />
+        <app-input-text
+          v-model="contactTelegram"
+          class="input mb-3"
+          placeholder="Telegram username HR"
+        />
+        <app-input-text
+          v-model="contactWhatsApp"
+          class="input mb-3"
+          placeholder="WhatsApp phone number HR"
+        />
+        <app-input-text v-model="contactPhone" class="input mb-3" placeholder="Телефон HR" />
+        <app-button
+          @click="addNewInterview"
+          label="Create interview"
+          :disabled="disabledSaveButton"
+          :loading="loading"
+        ></app-button>
+      </template>
+    </app-card>
+  </div>
 </template>
+
+<style scoped>
+.input {
+  width: 100%;
+}
+.content-interview {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
